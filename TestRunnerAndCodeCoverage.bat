6ca264f6cadd7d6948666a517a72dd35c2fbfbe3
PEDRO ESCAMILLA <pescamil@ford.com>
2023-11-22 09:22:24 -0600

    update jenkins files

---
+++ TestRunnerAndCodeCoverage.bat
@@ -0,0 +1,11 @@
+set ue5Location=%~1
+set workspace=%~2
+set projectFilename=%~3
+set testSuiteToRun=%~4
+set testReportFolder=%~5
+set testsLogName=%~6
+set codeCoverageFile=%~7
+
+set testRunnerCommand="%ue5Location%\Engine\Binaries\Win64\UE4Editor-cmd.exe" "%workspace%\%projectFilename%" -nosplash -Unattended -nopause -nosound -NullRHI -nocontentbrowser -ExecCmds="Automation RunTests %testSuiteToRun%;quit" -TestExit="Automation Test Queue Empty" -ReportOutputPath="%workspace%\%testReportFolder%" -log -Log=%testsLogName%
+
+"C:\Program Files\OpenCppCoverage\opencppcoverage.exe" --sources=\Source --modules %workspace% --excluded_sources=\Tests --export_type=cobertura:%codeCoverageFile%  -- %testRunnerCommand%

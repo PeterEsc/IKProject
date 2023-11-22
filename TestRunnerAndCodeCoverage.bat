49b33b54b29227fed24f1e7b9eba9d8b4130a491
PEDRO ESCAMILLA <pescamil@ford.com>
2023-11-22 14:21:07 -0600

    Update TestRunnerAndCodeCoverage.bat

--- TestRunnerAndCodeCoverage.bat
+++ TestRunnerAndCodeCoverage.bat
@@ -8,4 +8,4 @@ set codeCoverageFile=%~7
 
 set testRunnerCommand="%ue5Location%\Engine\Binaries\Win64\UnrealEditor-cmd.exe" "%workspace%\%projectFilename%" -nosplash -Unattended -nopause -NullRHI -nosound -nocontentbrowser -server -ExecCmds="Automation RunAll;quit" -TestExit="Automation Test Queue Empty" -ReportOutputPath="%workspace%\%testReportFolder%" -log -Log=%testsLogName%
 
-"C:\Program Files\OpenCppCoverage\opencppcoverage.exe" --sources=\Source --modules %workspace% --excluded_sources=\Tests --export_type=cobertura:%codeCoverageFile% --optimized_build  -- %testRunnerCommand%
+"C:\Program Files\OpenCppCoverage\opencppcoverage.exe" --sources=\Source --modules %workspace% --excluded_sources=\Tests --export_type=cobertura:%codeCoverageFile% --optimized_build  -- %testRunnerCommand% --verbose

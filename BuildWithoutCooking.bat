2d72fbd2bafa145599bd7419442078ad55ca87c5
PEDRO ESCAMILLA <pescamil@ford.com>
2023-11-22 15:04:38 -0600

    Update BuildWithoutCooking.bat

--- BuildWithoutCooking.bat
+++ BuildWithoutCooking.bat
@@ -3,4 +3,4 @@ set workspace=%~2
 set projectFilename=%~3
 
 
-"%ue5Location%\Engine\\Build\\BatchFiles\\RunUAT.bat BuildCookRun -nocompileeditor -installed -nop4 -project=%workspace%\\%projectFilename%.uproject -cook -stage -archive -archivedirectory=%workspace%\\dist -package -ue4exe="%ue5Location%\Engine\\Binaries\\Win64\\UE4Editor-Cmd.exe -ddc=DerivedDataBackendGraph -pak -prereqs -distribution -nodebuginfo -targetplatform=Win64 -build -target=%projectFilename% -clientconfig=Development -utf8output"
+"%ue5Location%\Engine\Build\BatchFiles\RunUAT.bat BuildCookRun -nocompileeditor -installed -nop4 -project=%workspace%\%projectFilename%.uproject -cook -stage -archive -archivedirectory=%workspace%\dist -package -ue5exe="%ue5Location%\Engine\Binaries\Win64\UnrealEditor-Cmd.exe -ddc=DerivedDataBackendGraph -pak -prereqs -distribution -nodebuginfo -targetplatform=Win64 -build -target=%projectFilename% -clientconfig=Development -utf8output"
